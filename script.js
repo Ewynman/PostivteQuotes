030a0732ec7004e995d3c20607990b4299290e66
Eddie Wynman <edward.wynman@gmail.com>
2023-06-07 10:42:28 -0400

    Random quotes, update links

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 $(document).ready(function () {
-    // typing text animation script
-    var typed = new Typed(".typing", {
-      strings: getQuotes(),
-      typeSpeed: 100,
-      backSpeed: 60,
-      loop: true,
-    });
-
-    // function to read quotes from text file
-    function getQuotes() {
-      var quotes = [];
-      $.ajax({
-        url: "quotes.txt",
-        async: false,
-        success: function (data) {
-          quotes = data.split("\n");
-        },
-        error: function () {
-          console.log("Error reading quotes file.");
-        },
-      });
-      return quotes;
-    }
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: shuffleArray(getQuotes()),
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
   });
+
+  // function to read quotes from text file
+  function getQuotes() {
+    var quotes = [];
+    $.ajax({
+      url: "quotes.txt",
+      async: false,
+      success: function (data) {
+        quotes = data.split("\n");
+      },
+      error: function () {
+        console.log("Error reading quotes file.");
+      },
+    });
+    return quotes;
+  }
+
+  // function to shuffle an array using Fisher-Yates algorithm
+  function shuffleArray(array) {
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+});
